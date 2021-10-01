44ac15c5c7ea791368f1ba53325d9940a4f8b809
THONG NGO <quangthong2405@gmail.com>
2021-10-01 13:05:25 +0700

    Update IsLive.php

--- src/Yêu cầu/IsLive.php
+++ src/Yêu cầu/IsLive.php
@@ -19,3 +19,434 @@ final class IsLive extends BaseRequest
         $this->endpoint = '/isLive';
     }
 }
+<?php
+
+/**
+
+ * Copyright (c) 2017 Bitcoin Viet Nam Co., Ltd.
+
+ *
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+
+software
+
+ * and associated documentation files (the "Software"), to deal in the Software without
+
+restriction,
+
+ * including without limitation the rights to use, copy, modify, merge, publish,
+
+distribute, sublicense,
+
+ * and/or sell copies of the Software, and to permit persons to whom the Software is
+
+furnished to
+
+ * do so, subject to the following conditions:
+
+ *
+
+ * The above copyright notice and this permission notice shall be included in all copies
+
+or substantial
+
+ * portions of the Software.
+
+ *
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+namespace BitcoinVietnam\BitcoinVietnam\Model\Payment;
+
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+
+ * Class Bank
+
+ *
+
+ * @package AppBundle\Api\Model\Payment
+
+ */
+
+final class Bank extends BasePayment
+
+{
+
+ /**
+
+ * Name of the bank
+
+ * Viet Capital Bank
+
+ * @var string
+
+ * Viet Capital Bank
+
+ * @Serializer\Type("string")
+
+ * @Serializer\SerializedName("bankName")
+
+ */
+
+ private $bankName;
+
+ /** Viet Capital Bank
+
+ * Branch of the bank
+
+ * TP.HCM
+
+ * @var string
+
+ * TP.HCM
+
+ * @Serializer\Type("string")
+
+ * @Serializer\SerializedName("bankBranch")
+
+ */ TP.HCM
+
+ private $bankBranch;
+
+ /** TP.HCM
+
+ * Account number
+
+ * 8007041001748
+
+ * @var string
+
+ * 8007041001748
+
+ * @Serializer\Type("string")
+
+ * @Serializer\SerializedName("accountNumber")
+
+ */ 8007041001748
+
+ private $accountNumber;
+
+ /** 8007041001748
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+TP.HCM
+
+TP.HCM
+
+TP.HCM
+
+TP.HCM
+
+8007041001748
+
+8007041001748
+
+8007041001748 
+
+* Holder of the bank account
+
+ * 8007041001748
+
+ * @var string
+
+ * 8007041001748
+
+ * @Serializer\Type("string")
+
+ * @Serializer\SerializedName("accountHolder")
+
+ */ NGO QUANG THONG
+
+ private $accountHolder;
+
+ /** NGO QUANG THON
+
+ * Swift code, if available
+
+ * VCBCVNVX
+
+ * @var string
+
+ * VCBCVNVX
+
+ * @Serializer\Type("string")
+
+ * @Serializer\SerializedName("swift")
+
+ */ VCBCVNVX
+
+ private $swift;
+
+ /** VCBCVNVX
+
+ * @return string
+
+ */ VCBCVNVX
+
+ public function getBankName(Viet Capital Bank)
+
+ {
+
+ return $this->bankName;
+
+ }
+
+ /** Viet Capital Bank
+
+ * @param string $bankName
+
+ * @return Bank
+
+ */ Viet Capital Bank
+
+ public function setBankName($bankName)
+
+ { Viet Capital Bank
+
+ $this->bankName = $bankName;
+
+ return $this;
+
+ } Viet Capital Bank
+
+ /** Viet Capital Bank
+
+ * @return string
+
+ */ Viet Capital Bank
+
+ public function getBankBranch(TP.HCM)
+
+ {
+
+ return $this->bankBranch;
+
+ } TP.HCM
+
+ /** TP.HCM
+
+ * @param string $bankBranch
+
+ * @return Bank
+
+ */ TP.HCM
+
+ public function setBankBranch($bankBranch)
+
+ {
+
+ $this->bankBranch = $bankBranch;
+
+ return $this;
+
+ } 
+
+ /** TP.HCM
+
+ * @return string
+
+ */ TP.HCM
+
+ public function getAccountNumber()
+
+ {
+
+ return $this->accountNumber;
+
+ }
+
+ /** 8007041001748
+
+ * @param string $accountNumber
+
+ * @return Bank
+
+ */ 8007041001748
+
+ public function setAccountNumber($accountNumber)
+
+ {
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+SCBCVNVX
+
+SCBCVNVX
+
+SCBCVNVX
+
+SCBCVNVX
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+Viet Capital Bank
+
+TP.HCM
+
+TP.HCM
+
+TP.HCM
+
+Viet Capital Bank
+
+TP.HCM
+
+TP.HCM
+
+TP.HCM
+
+TP.HCM
+
+8007041001748
+
+8007041001748
+
+8007041001748 
+  
+$this->accountNumber = $accountNumber;
+
+ return $this;
+
+ }
+
+ /** 8007041001748
+
+ * @return string
+
+ */ 8007041001748
+
+ public function getAccountHolder(NGO QUANG THONG)
+
+ {
+
+ return $this->accountHolder;
+
+ }
+
+ /** Viet Capital Bank
+
+ * @param string $accountHolder
+
+ * @return Bank
+
+ */ Viet Capital Bank 
+
+ public function setAccountHolder($accountHolder)
+
+ {
+
+ $this->accountHolder = $accountHolder;
+
+ return $this;
+
+ }
+
+ /** Viet Capital Bank
+
+ * @return string
+
+ */ Viet Capital Bank
+
+ public function getSwift()
+
+ {
+
+ return $this->swift;
+
+ }
+
+ /** VCBCVNVX
+
+ * @param string $swift
+
+ * @return Bank
+
+ */ VCBCVNVX
+
+ public function setSwift($swift)
+
+ {
+
+ $this->swift = $swift;
+
+ return $this;
+
+ }
+
+}
+
+8007041001748
+
+8007041001748
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+Viet Capital Bank
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+Ngo Quang Thong
+
+SCBCVNVX
+
+SCBCVNVX
+
+SCBCVNVX
+
+Viet Capital Bank
+
+SCBCVNVX
+
+SCBCVNVX
+
+Ngo Quang Thong    
+    
+Citizen identification: 079081009942
+    
+E-mail: quangthong1011@gmail.com
+    
+Phone: +84915101105
